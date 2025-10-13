215021095b6516e871933331c2c54bba97ab379e
gasizdat <gasizdat@gmail.com>
2025-10-13 11:15:56 +0300

    (+) Add ProjectListItem

--- UI/App/OpenOrCreateDialog.xaml.cs
+++ UI/App/OpenOrCreateDialog.xaml.cs
@@ -1,9 +1,21 @@
+using System.Collections.ObjectModel;
+
 namespace GT4.UI;
 
 public partial class OpenOrCreateDialog : ContentPage
 {
-	public OpenOrCreateDialog()
-	{
-		InitializeComponent();
-	}
+  public OpenOrCreateDialog()
+  {
+    InitializeComponent();
+    BindingContext = this;
+  }
+
+  public string DialogTitle => "Open or Create a Genealogy";
+  public string DialogHint => "You can open the existent or create a new Genealogy Tree";
+  public ObservableCollection<ProjectListItem> Projects => new ObservableCollection<ProjectListItem>
+        {
+            new ProjectListItem { },
+            new ProjectListItem { },
+            new ProjectListItem { }
+        };
 }
---
+++ UI/App/ProjectListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace GT4.UI
+{
+  public class ProjectListItem
+  {
+    public string Name => "Some Project Name";
+    public string Path => "/path/to/project.file";
+  }
+}
