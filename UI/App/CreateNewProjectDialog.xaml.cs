b745909a93cbc68d34ef1bf866228c6e5d97bae2
gasizdat <gasizdat@gmail.com>
2025-10-17 13:20:02 +0300

    (*) Change the name of the Create Project Dialog button accordingly to the user input.

--- UI/App/CreateNewProjectDialog.xaml.cs
+++ UI/App/CreateNewProjectDialog.xaml.cs
@@ -2,6 +2,7 @@ namespace GT4.UI;
 
 public partial class CreateNewProjectDialog : ContentPage
 {
+  private string _projectName = string.Empty;
   private TaskCompletionSource<Project.ProjectInfo> _info = new();
 
   public CreateNewProjectDialog()
@@ -17,10 +18,18 @@ public partial class CreateNewProjectDialog : ContentPage
 
   public Task<Project.ProjectInfo> ProjectInfo => _info.Task;
 
-  public string CreateProjectBtName => "Create Genealogy Tree";
+  public string CreateProjectBtName => string.IsNullOrWhiteSpace(ProjectName) ? "Cancel" : "Create Genealogy Tree";
   public string DialogTitle => "Create a Genealogy Tree";
   public string ProjectDescriptionPlaceholder => "Enter a description for the new Genealogy Tree";
   public string ProjectDescription { get; set; } = string.Empty;
   public string ProjectNamePlaceholder => "Enter a name for the new Genealogy Tree";
-  public string ProjectName { get; set; } = string.Empty;
+  public string ProjectName
+  {
+    get => _projectName;
+    set
+    {
+      _projectName = value;
+      OnPropertyChanged(nameof(CreateProjectBtName));
+    }
+  }
 }
