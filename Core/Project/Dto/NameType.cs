567341443e3ae7f120d428a6508c1ed262e604d3
gasizdat <gasizdat@gmail.com>
2025-10-19 21:51:03 +0300

    (*) Make NameType flags.

--- Core/Project/Dto/NameType.cs
+++ Core/Project/Dto/NameType.cs
@@ -1,21 +1,22 @@
 ﻿namespace GT4.Core.Project.Dto;
 
+[Flags]
 public enum NameType
 {
   // For the internal use only
   AllNames = 0,
   // The name of a family
-  FamilyName = 1,
+  FamilyName = 0x1,
   // The first name of a person
-  FirstName = 2,
+  FirstName = 0x2,
   // The optional middle name of the person
-  MiddleName = 3,
+  MiddleName = 0x4,
   // The optional last name of the person
-  LastName = 4,
+  LastName = 0x8,
   // The optional additional name (e.g., nickname, alias)
-  AdditionalName = 5,
+  AdditionalName = 0xf,
   // The declension of a name in the Male gender (For languages ​​with gender declension)
-  MaleName = 6,
+  MaleDeclension = 0x10,
   // The declension of a name in the Female gender (For languages ​​with gender declension)
-  FemaleName = 7,
+  FemaleDeclension = 0x20,
 }
