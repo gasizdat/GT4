d657b5792f15e6cfb3bbc2119b9b78f405529aa1
gasizdat <gasizdat@gmail.com>
2025-10-24 20:03:56 +0300

    (+) Add FamiliPage

---
+++ UI/App/FamilyPage.xaml.cs
@@ -0,0 +1,61 @@
+namespace GT4.UI;
+
+using GT4.Core.Project;
+using GT4.Core.Project.Dto;
+
+[QueryProperty(nameof(FamilyName), "FamilyName")]
+public partial class FamilyPage : ContentPage
+{
+  private Name? _FamilyName = null;
+
+  public FamilyPage()
+  {
+    InitializeComponent();
+    BindingContext = this;
+  }
+
+  public Name? FamilyName
+  {
+    get => _FamilyName;
+    set
+    {
+      _FamilyName = value;
+      OnPropertyChanged(nameof(Members));
+    }
+  }
+
+  public ServiceProvider Services { get; set; } = ServiceBuilder.DefaultServices;
+ 
+  public ICollection<PersonInfoItem> Members
+  {
+    get
+    {
+      if (FamilyName is null)
+      {
+        return [];
+      }
+
+      try
+      {
+        using var token = new Core.Utils.DefaultCancellationToken();
+        var nameFormatter = Services.GetRequiredService<INameFormatter>();
+        return Services.GetRequiredService<ICurrentProjectProvider>()
+          .Project
+          .Persons
+          .GetPersonsByNameAsync(FamilyName, token)
+          .Result
+          .Select(person => new PersonInfoItem(person, nameFormatter))
+          .ToArray();
+      }
+      catch (Exception ex)
+      {
+        return [/*new FamilyInfoItemRefresh(ex)*/];
+      }
+    }
+  }
+
+  internal async void OnMemberSelected(object sender, SelectionChangedEventArgs e)
+  {
+
+  }
+}
