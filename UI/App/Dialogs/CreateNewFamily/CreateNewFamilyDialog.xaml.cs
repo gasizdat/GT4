9e6db81739c8d2b15efc76bd6c500631bf639f4d
gasizdat <gasizdat@gmail.com>
2025-10-25 19:51:57 +0300

    (-) Fix CreateNewFamilyDialog cancellation behavior.

--- UI/App/Dialogs/CreateNewFamily/CreateNewFamilyDialog.xaml.cs
+++ UI/App/Dialogs/CreateNewFamily/CreateNewFamilyDialog.xaml.cs
@@ -6,10 +6,13 @@ public partial class CreateNewFamilyDialog : ContentPage
 {
   public record FamilyInfo(string Name, string MaleLastName, string FemaleLastName);
 
-  private TaskCompletionSource<FamilyInfo?> _Info = new();
+  private readonly TaskCompletionSource<FamilyInfo?> _Info = new(null);
   private string _FamilyName = string.Empty;
   private string _MaleLastName = string.Empty;
   private string _FemaleLastName = string.Empty;
+  private bool _NotReady => string.IsNullOrWhiteSpace(FamilyName) ||
+    string.IsNullOrWhiteSpace(MaleLastName) ||
+    string.IsNullOrWhiteSpace(FemaleLastName);
 
   public CreateNewFamilyDialog()
   {
@@ -32,10 +35,10 @@ public partial class CreateNewFamilyDialog : ContentPage
   public string MaleLastName { get => _MaleLastName; set => _MaleLastName = value; }
   public string FemaleLastName { get => _FemaleLastName; set => _FemaleLastName = value; }
   public Task<FamilyInfo?> Info => _Info.Task;
-  public string CreateFamilyBtnName => string.IsNullOrWhiteSpace(FamilyName) ? UIStrings.BtnNameCancel : UIStrings.BtnNameCreateFamily;
+  public string CreateFamilyBtnName => _NotReady ? UIStrings.BtnNameCancel : UIStrings.BtnNameCreateFamily;
 
   public void OnCreateFamilyBtn(object sender, EventArgs e)
   {
-    _Info.SetResult(new(FamilyName, MaleLastName, FemaleLastName));
+    _Info.SetResult(_NotReady ? null : new(FamilyName, MaleLastName, FemaleLastName));
   }
 }
