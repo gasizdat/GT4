13d5dc1e6ecaaea1d9f0b4309625cfc4ba90c53a
gasizdat <gasizdat@gmail.com>
2025-10-30 21:22:26 +0200

    [DRAFT] (+) Add RelationshipTypeFormatter

---
+++ UI/App/RelationshipTypeFormatter.cs
@@ -0,0 +1,35 @@
+﻿using GT4.Core.Project.Dto;
+
+namespace GT4.UI;
+
+public class RelationshipTypeFormatter : IRelationshipTypeFormatter
+{
+  public string GetRelationshipTypeName(RelationshipType type)
+  {
+    switch (type)
+    {
+      case RelationshipType.Mother:
+        return "Mother";
+      case RelationshipType.Father:
+        return "Father";
+      case RelationshipType.AdoptiveMother:
+        return "Adoptive Mother";
+      case RelationshipType.AdoptiveFather:
+        return "Adoptive Father";
+      case RelationshipType.Spouse:
+        return "Spouse";
+      case RelationshipType.Wife:
+        return "Wife";
+      case RelationshipType.Husband:
+        return "Husband";
+      case RelationshipType.Son:
+        return "Son";
+      case RelationshipType.Daughter:
+        return "Daughter";
+      case RelationshipType.Child:
+        return "Child";
+      default:
+        return "Unknown";
+    }
+  }
+}
