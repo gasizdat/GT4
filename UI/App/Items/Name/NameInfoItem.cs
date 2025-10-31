bfb365c780102ca8c58018d3413fefeb4f2b45ac
gasizdat <gasizdat@gmail.com>
2025-10-31 20:52:25 +0200

    (+) Add auxiliary components to CreateNewPersonDialog

---
+++ UI/App/Items/Name/NameInfoItem.cs
@@ -0,0 +1,36 @@
+﻿using GT4.Core.Project.Dto;
+using GT4.UI.Resources;
+
+namespace GT4.UI.App.Items;
+
+public class NameInfoItem : CollectionItemBase<Name>
+{
+  public NameInfoItem(Name name)
+    : base(name, string.Empty)
+  {
+  }
+
+  public string Value => Info.Value;
+  public string Type
+  {
+    get
+    {
+      switch (Info.Type)
+      {
+        case NameType.FirstName:
+          return UIStrings.FieldFirstName;
+        case NameType.LastName:
+          return UIStrings.FieldLastName;
+        case NameType.MiddleName:
+          return UIStrings.FieldMiddleName;
+        case NameType.FamilyName:
+          return UIStrings.FieldFamilyName;
+        case NameType.AdditionalName:
+          return UIStrings.FieldAdditionalName;
+
+        default:
+          return string.Empty;
+      }
+    }
+  }
+}
---
+++ UI/App/Items/RelativeMember/RelativeMemberInfoItem.cs
@@ -0,0 +1,20 @@
+﻿using GT4.Core.Project.Dto;
+
+namespace GT4.UI.App.Items;
+
+public class RelativeMemberInfoItem : CollectionItemBase<Relative>
+{
+  private readonly IDateFormatter _DateFormatter;
+  private readonly IRelationshipTypeFormatter _RelationshipTypeFormatter;
+
+  public RelativeMemberInfoItem(Relative relative, ServiceProvider services)
+    : base(relative, "wife.png")
+  {
+    _DateFormatter = services.GetRequiredService<IDateFormatter>();
+    _RelationshipTypeFormatter = services.GetRequiredService<IRelationshipTypeFormatter>();
+  }
+
+  public bool ShowDate => Info.Date.HasValue;
+  public string Date => _DateFormatter.ToString(Info.Date);
+  public string RelationTypeName => _RelationshipTypeFormatter.GetRelationshipTypeName(Info.Type);
+}
