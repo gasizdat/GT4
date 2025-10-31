5712fd9335c29bdab27a2b6ffd9b6fbce1e4eea7
gasizdat <gasizdat@gmail.com>
2025-10-31 20:42:58 +0200

    (+) Add DateInfoView component.

---
+++ UI/App/Components/DateInfoView.xaml.cs
@@ -0,0 +1,33 @@
+using GT4.Core.Utils;
+
+namespace GT4.UI.App.Components;
+
+public partial class DateInfoView : ContentView
+{
+  private readonly IDateFormatter _Formatter = ServiceBuilder.DefaultServices.GetRequiredService<IDateFormatter>();
+
+  public DateInfoView()
+  {
+    InitializeComponent();
+  }
+
+  public static readonly BindableProperty CaptionProperty =
+    BindableProperty.Create(nameof(Caption), typeof(string), typeof(DateInfoView), default, BindingMode.OneWay);
+
+  public static readonly BindableProperty DateProperty =
+    BindableProperty.Create(nameof(Date), typeof(Date?), typeof(DateInfoView), null, BindingMode.OneWay, null, OnDateChanged);
+
+  public Date? Date => (Date?)GetValue(DateProperty);
+
+  public string Caption => (string)GetValue(CaptionProperty);
+
+  public string DateText => _Formatter.ToString(Date);
+
+  private static void OnDateChanged(BindableObject obj, object oldValue, object newValue)
+  {
+    if (obj is DateInfoView view && oldValue != newValue)
+    {
+      view.OnPropertyChanged(nameof(DateText));
+    }
+  }
+}
