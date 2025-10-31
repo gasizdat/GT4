4fc15786abe26b9e9823150af88fe3200e663f68
gasizdat <gasizdat@gmail.com>
2025-10-31 20:50:00 +0200

    (~) Extract ImageSource creation methods into ImageUtils.

---
+++ UI/App/ImageUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace GT4.UI;
+
+public static class ImageUtils
+{
+  public static ImageSource ImageFromBytes(byte[] data) =>
+    ImageSource.FromStream(token => Task.Run<Stream>(() => new MemoryStream(data), token));
+
+  public static ImageSource ImageFromRawResource(string resourceName) =>
+    ImageSource.FromStream(_ => FileSystem.OpenAppPackageFileAsync(resourceName));
+}
--- UI/App/Items/Generic/CollectionItemBase.cs
+++ UI/App/Items/Generic/CollectionItemBase.cs
@@ -6,23 +6,17 @@ public abstract class CollectionItemBase<TDto>
 
   protected CollectionItemBase(TDto info, string defaultImageResource)
   {
-    DefaultImage = ImageFromRawResource(defaultImageResource);
+    DefaultImage = ImageUtils.ImageFromRawResource(defaultImageResource);
     _Info = info;
   }
 
   protected virtual ImageSource? CustomImage => null;
   protected readonly ImageSource DefaultImage;
-  protected readonly ImageSource CreateItemImage = ImageFromRawResource("add_content.png");
-  protected readonly ImageSource RefreshItemImage = ImageFromRawResource("refresh_on_error.png");
+  protected readonly ImageSource CreateItemImage = ImageUtils.ImageFromRawResource("add_content.png");
+  protected readonly ImageSource RefreshItemImage = ImageUtils.ImageFromRawResource("refresh_on_error.png");
 
   protected static string GetRefreshOnErrorButtonName(Exception ex) =>
     string.Format(Resources.UIStrings.BtnNameRefreshAfterError, ex.Message);
-
-  protected static ImageSource ImageFromBytes(byte[] data) =>
-    ImageSource.FromStream(token => Task.Run<Stream>(() => new MemoryStream(data), token));
-
-  protected static ImageSource ImageFromRawResource(string resourceName) =>
-    ImageSource.FromStream(_ => FileSystem.OpenAppPackageFileAsync(resourceName));
 
   public TDto Info => _Info;
   public ImageSource Icon => CustomImage ?? DefaultImage;
