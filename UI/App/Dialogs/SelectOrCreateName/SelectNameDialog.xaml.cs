1365b85ef0523e17a7bbdb467e0b2cad0725faab
gasizdat <gasizdat@gmail.com>
2025-11-01 22:01:19 +0200

    (*) Improve SelectNameDialog

--- UI/App/Dialogs/SelectOrCreateName/SelectNameDialog.xaml.cs
+++ UI/App/Dialogs/SelectOrCreateName/SelectNameDialog.xaml.cs
@@ -32,8 +32,6 @@ public partial class SelectNameDialog : ContentPage
       .Select(type => new NameTypeInfoItem(_NameTypeFormatter.ToString(type), type)));
     _CurrentNameType = _NameTypes.First();
 
-    InitializeComponent();
-
     switch (biologicalSex)
     {
       case BiologicalSex.Male:
@@ -45,8 +43,9 @@ public partial class SelectNameDialog : ContentPage
       default:
         _NameDeclension = NameType.AllNames;
         break;
-
     }
+
+    InitializeComponent();
   }
 
   public ICollection<NameTypeInfoItem> NameTypes => _NameTypes;
@@ -75,6 +74,7 @@ public partial class SelectNameDialog : ContentPage
 
       _CurrentNameType = value;
       OnPropertyChanged(nameof(Names));
+      CurrentName = null;
     }
   }
 
@@ -85,6 +85,66 @@ public partial class SelectNameDialog : ContentPage
     {
       _CurrentName = value;
       OnPropertyChanged(nameof(SelectNameBtnName));
+    }
+  }
+
+  public async void OnAddNameBtn(object sender, EventArgs e)
+  {
+    NameType dialogNameType;
+    switch (_CurrentNameType.Type)
+    {
+      case NameType.MiddleName:
+        dialogNameType = NameType.FirstName | NameType.MaleDeclension;
+        break;
+      case NameType.FirstName:
+        dialogNameType = NameType.FirstName | _NameDeclension;
+        break;
+      case NameType.LastName:
+        dialogNameType = NameType.FamilyName;
+        break;
+      case NameType.AdditionalName:
+      default:
+        return;
+    }
+
+    var dialog = new CreateNewNameDialog(dialogNameType);
+
+    await Navigation.PushModalAsync(dialog);
+    var info = await dialog.Info;
+    await Navigation.PopModalAsync();
+
+    if (info is null)
+      return;
+
+    try
+    {
+      using var token = _CancellationTokenProvider.CreateDbCancellationToken();
+      var project = _CurrentProjectProvider.Project;
+      Name name;
+      switch (dialogNameType)
+      {
+        case NameType.FamilyName:
+          name = await project
+            .Family
+            .AddFamilyAsync(familyName: info.Name, maleLastName: info.MaleName, femaleLastName: info.FemaleName, token);
+          break;
+
+        case NameType.FirstName | NameType.MaleDeclension:
+          name = await project.Names.AddFirstMaleNameAsync(firstName: info.Name, maleMiddleName: info.MaleName, femaleMiddleName: info.FemaleName, token);
+          break;
+        case NameType.FirstName | NameType.FemaleDeclension:
+          name = await project.Names.AddFirstFemaleNameAsync(info.Name, token);
+          break;
+        default:
+          throw new ApplicationException(nameof(OnAddNameBtn));
+      }
+      OnPropertyChanged(nameof(Names));
+
+      // TODO select created name
+    }
+    catch (Exception ex)
+    {
+      await DisplayAlert(UIStrings.AlertTitleError, ex.Message, UIStrings.BtnNameOk);
     }
   }
 
