a7a4ce3bc681c9ea4e978608899499916b39ad9e
gasizdat <gasizdat@gmail.com>
2025-11-01 11:37:26 +0200

    (+) Add addorner command parameter class.

---
+++ UI/App/Components/AdornerCommandParameter.cs
@@ -0,0 +1,22 @@
+﻿namespace GT4.UI.App.Components;
+
+public class AdornerCommandParameter : BindableObject
+{
+  public static readonly BindableProperty ElementProperty =
+          BindableProperty.Create(nameof(Element), typeof(object), typeof(AdornerCommandParameter));
+  public static readonly BindableProperty CommandNameProperty =
+          BindableProperty.Create(nameof(CommandName), typeof(string), typeof(AdornerCommandParameter));
+
+  public object Element
+  {
+    get => GetValue(ElementProperty);
+    set => SetValue(ElementProperty, value);
+  }
+
+  public string CommandName
+  {
+    get => (string)GetValue(CommandNameProperty);
+    set => SetValue(CommandNameProperty, value);
+  }
+}
+
