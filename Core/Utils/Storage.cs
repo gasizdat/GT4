d5f151b47293412039d14146d01b0df766e8cdaa
gasizdat <gasizdat@gmail.com>
2025-11-02 16:57:43 +0200

    (*) Add minor improvements.

--- Core/Utils/Storage.cs
+++ Core/Utils/Storage.cs
@@ -2,7 +2,7 @@
 
 internal class Storage : IStorage
 {
-  private const string AppId = "{2985F7F6-693A-4306-9135-5A955A855F3E}";
+  private const string AppId = "GenTree v4";
 
   public string ApplicationData => Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
   public string MyDocuments => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
