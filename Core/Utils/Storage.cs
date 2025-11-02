d8f52ae27e3724956022bea8d730edb54ecaa5c9
gasizdat <gasizdat@gmail.com>
2025-11-02 16:09:59 +0200

    (*) Store GT projects in user personal documents

--- Core/Utils/Storage.cs
+++ Core/Utils/Storage.cs
@@ -5,5 +5,6 @@ internal class Storage : IStorage
   private const string AppId = "{2985F7F6-693A-4306-9135-5A955A855F3E}";
 
   public string ApplicationData => Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-  public string ProjectsRoot => Path.Combine(ApplicationData, AppId, "local_projects");
+  public string MyDocuments => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+  public string ProjectsRoot => Path.Combine(MyDocuments, AppId, "local_projects");
 }
