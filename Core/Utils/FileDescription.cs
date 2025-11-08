361c25d818662a5937d6e3734396e20a8f591d0d
gasizdat <gasizdat@gmail.com>
2025-11-09 00:49:06 +0200

    (-) Fix FileDescription. Exclude MIME type from comparison.

--- Core/Utils/FileDescription.cs
+++ Core/Utils/FileDescription.cs
@@ -1,5 +1,12 @@
 ﻿namespace GT4.Core.Utils;
 
+/// <summary>
+/// Describes a file
+/// </summary>
+/// <param name="Directory"> The file directory </param>
+/// <param name="FileName"> The File name </param>
+/// <param name="MimeType"> The file MIME type (optional) </param>
+/// <remarks> MimeType is not considered in hash code generation or equality comparison </remarks>
 public record class FileDescription(
   DirectoryDescription Directory,
   string FileName,
@@ -8,14 +15,11 @@ public record class FileDescription(
 {
   public override int GetHashCode()
   {
-    return HashCode.Combine(
-      Directory.GetHashCode(),
-      FileName.GetHashCode(),
-      MimeType?.GetHashCode());
+    return HashCode.Combine(Directory.GetHashCode(), FileName.GetHashCode());
   }
 
   public virtual bool Equals(FileDescription? other)
   {
-    return Directory == other?.Directory && FileName == other?.FileName && MimeType == other?.MimeType;
+    return Directory == other?.Directory && FileName == other?.FileName;
   }
 }
