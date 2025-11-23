2e30e6918aef8b00146739bcfb26f9d6a35154cb
gasizdat <gasizdat@gmail.com>
2025-11-23 23:11:41 +0200

    (+) Add ElementIdComparer.

---
+++ Core/Project/ElementIdComparer.cs
@@ -0,0 +1,17 @@
+﻿using GT4.Core.Project.Dto;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GT4.Core.Project;
+
+internal class ElementIdComparer<TSource> : IEqualityComparer<TSource> where TSource : ElementId
+{
+  public bool Equals(TSource? x, TSource? y)
+  {
+    return x?.Id == y?.Id;
+  }
+
+  public int GetHashCode([DisallowNull] TSource obj)
+  {
+    return obj.Id;
+  }
+}
