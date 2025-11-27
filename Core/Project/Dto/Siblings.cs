9f99e8b0b1b74167a4c470f93b55839efa8caccb
gasizdat <gasizdat@gmail.com>
2025-11-27 12:20:38 +0200

    (*) Remove SiblingsInfo record. Update Siblings record.

--- Core/Project/Dto/Siblings.cs
+++ Core/Project/Dto/Siblings.cs
@@ -1,3 +1,9 @@
 ﻿namespace GT4.Core.Project.Dto;
 
-public record class Siblings();
+public record class Siblings(
+  RelativeInfo[] Native,
+  RelativeInfo[] ByFather,
+  RelativeInfo[] ByMother,
+  RelativeInfo[] Adoptive,
+  RelativeInfo[] Step
+);
