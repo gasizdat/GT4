4e1490d6fa09ca18c65625b6fa587a245fab4a58
gasizdat <gasizdat@gmail.com>
2025-11-30 18:02:39 +0200

    (*) Add ImageUtils.ToBytesAsync.

--- UI/App/ImageUtils.cs
+++ UI/App/ImageUtils.cs
@@ -2,6 +2,19 @@
 
 public static class ImageUtils
 {
+  private static async Task<byte[]?> ToBytesAsync(Stream? stream, CancellationToken token)
+  {
+    if (stream is null)
+    {
+      return null;
+    }
+
+    using var ms = new MemoryStream();
+    await stream.CopyToAsync(ms, token).ConfigureAwait(false);
+
+    return ms.ToArray();
+  }
+
   public static ImageSource ImageFromBytes(byte[] data) =>
     ImageSource.FromStream(token => Task.Run<Stream>(() => new MemoryStream(data), token));
 
@@ -34,15 +47,18 @@ public static class ImageUtils
 
     if (stream == null)
     {
-      throw new InvalidOperationException(
-        $"Could not obtain a stream from {source.GetType().Name}."
-      );
+      throw new InvalidOperationException($"Could not obtain a stream from {source.GetType().Name}.");
     }
     using (stream)
-    using (var ms = new MemoryStream())
     {
-      await stream.CopyToAsync(ms, token).ConfigureAwait(false);
-      return ms.ToArray();
+      return await ToBytesAsync(stream, token);
     }
   }
+
+  public static async Task<byte[]?> ToBytesAsync(string resourceName, CancellationToken token)
+  {
+    using var stream = await FileSystem.OpenAppPackageFileAsync(resourceName);
+    return await ToBytesAsync(stream, token);
+  }
+
 }
