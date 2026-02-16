68489433e6ed6afab9847d6ccad84ea767c74124
gasizdat <gasizdat@gmail.com>
2026-02-16 21:26:22 +0200

    (+) Add RelationshipTypeFormatterTests unut tests.

---
+++ Tests/GT4.UI.Utils.Tests/RelationshipTypeFormatterTests.cs
@@ -0,0 +1,47 @@
+﻿using GT4.Core.Project.Dto;
+using GT4.UI.Utils.Formatters;
+using System.Globalization;
+using Xunit;
+
+
+namespace GT4.UI.Utils.Tests;
+
+public class RelationshipTypeFormatterTests
+{
+  private readonly RelationshipTypeFormatter _formatter = new();
+
+  private static Generation? ToGeneration(int? generation) =>
+    generation.HasValue ? new Generation(generation.Value) : null;
+
+  private static Consanguinity? ToConsanguinity(int? consanguinity) =>
+    consanguinity.HasValue ? new Consanguinity(consanguinity.Value) : null;
+
+  private static void SetLanguageCode(string code)
+  {
+    var culture = new CultureInfo(code);
+    Thread.CurrentThread.CurrentCulture = culture;
+    Thread.CurrentThread.CurrentUICulture = culture;
+    CultureInfo.DefaultThreadCurrentCulture = culture;
+    CultureInfo.DefaultThreadCurrentUICulture = culture;
+  }
+
+  private static void SetEn() => SetLanguageCode("EN");
+  private static void SetRu() => SetLanguageCode("RU");
+
+  [Theory]
+  [InlineData(null, null, "Parent")]
+  [InlineData(1, null, "Parent")]
+  [InlineData(2, null, "Grandparent")]
+  [InlineData(4, null, "Great-great-grandparent")]
+  public void EN_UnknownSex_Parent(int? generation, int? consanguinity, string expected)
+  {
+    SetEn();
+    var actual = _formatter.ToString(
+      RelationshipType.Parent, 
+      BiologicalSex.Unknown, 
+      ToGeneration(generation), 
+      ToConsanguinity(consanguinity));
+
+    Assert.Equal(expected, actual);
+  }
+}
