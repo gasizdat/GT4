ca4aaf4a9fbb35a03a89d6bda8e54a06e05c0341
gasizdat <gasizdat@gmail.com>
2026-02-19 21:11:35 +0200

    (*) Expand relationship formatter tests for EN/RU, generations

--- Tests/GT4.UI.Utils.Tests/RelationshipTypeFormatterTests.cs
+++ Tests/GT4.UI.Utils.Tests/RelationshipTypeFormatterTests.cs
@@ -33,6 +33,7 @@ public class RelationshipTypeFormatterTests
   [InlineData(1, null, "Parent")]
   [InlineData(2, null, "Grandparent")]
   [InlineData(4, null, "Great-great-grandparent")]
+  [InlineData(40, null, "38-Great-grandparent")]
   public void EN_UnknownSex_Parent(int? generation, int? consanguinity, string expected)
   {
     SetEn();
@@ -40,6 +41,42 @@ public class RelationshipTypeFormatterTests
       RelationshipType.Parent, 
       BiologicalSex.Unknown, 
       ToGeneration(generation), 
+      ToConsanguinity(consanguinity));
+
+    Assert.Equal(expected, actual);
+  }
+
+  [Theory]
+  [InlineData(null, null, "Родитель")]
+  [InlineData(1, null, "Родитель")]
+  [InlineData(2, null, "Дедушка или бабушка")]
+  [InlineData(4, null, "Пра-пра-дедушка или бабушка")]
+  [InlineData(40, null, "38-Пра-дедушка или бабушка")]
+  public void RU_UnknownSex_Parent(int? generation, int? consanguinity, string expected)
+  {
+    SetRu();
+    var actual = _formatter.ToString(
+      RelationshipType.Parent,
+      BiologicalSex.Unknown,
+      ToGeneration(generation),
+      ToConsanguinity(consanguinity));
+
+    Assert.Equal(expected, actual);
+  }
+
+  [Theory]
+  [InlineData(null, null, "Child")]
+  [InlineData(-1, null, "Child")]
+  [InlineData(-2, null, "Grandchild")]
+  [InlineData(-4, null, "Great-great-grandchild")]
+  [InlineData(-15, null, "13-Great-grandchild")]
+  public void EN_UnknownSex_Child(int? generation, int? consanguinity, string expected)
+  {
+    SetEn();
+    var actual = _formatter.ToString(
+      RelationshipType.Child,
+      BiologicalSex.Unknown,
+      ToGeneration(generation),
       ToConsanguinity(consanguinity));
 
     Assert.Equal(expected, actual);
