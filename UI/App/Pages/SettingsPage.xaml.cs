aa5dab3d91da50b186523440438086d0281ab5df
gasizdat <gasizdat@gmail.com>
2026-04-25 23:55:49 +0300

    (*) Make SettingsPage universal

--- UI/App/Pages/SettingsPage.xaml.cs
+++ UI/App/Pages/SettingsPage.xaml.cs
@@ -1,6 +1,4 @@
 using GT4.Core.Utils;
-using GT4.UI.Resources;
-using GT4.UI.Utils.Formatters;
 
 namespace GT4.UI.Pages;
 
@@ -15,41 +13,8 @@ public partial class SettingsPage : ContentPage
     InitializeComponent();
   }
 
-  public string DateExample
-  {
-    // TODO temporary code
-    get => _SettingEditors.Single(s => s.DisplayName == UIStrings.FieldDateDisplayFormat).Example;
-  }
-
-  public string DateFormat
-  {
-    // TODO temporary code
-    get => _SettingEditors.Single(s => s.DisplayName == UIStrings.FieldDateDisplayFormat).Value;
-    set
-    {
-      // TODO temporary code
-      _SettingEditors.Single(s => s.DisplayName == UIStrings.FieldDateDisplayFormat).Value = value;
-      OnPropertyChanged(nameof(DateFormat));
-      OnPropertyChanged(nameof(DateExample));
-    }
-  }
-
-  public string ShortDateExample
-  {
-    // TODO temporary code
-    get => _SettingEditors.Single(s => s.DisplayName == UIStrings.FieldShortDateDisplayFormat).Example;
-  }
-
-  public string ShortDateFormat
-  {
-    // TODO temporary code
-    get => _SettingEditors.Single(s => s.DisplayName == UIStrings.FieldShortDateDisplayFormat).Value;
-    // TODO temporary code
-    set
-    {
-      _SettingEditors.Single(s => s.DisplayName == UIStrings.FieldShortDateDisplayFormat).Value = value;
-      OnPropertyChanged(nameof(ShortDateFormat));
-      OnPropertyChanged(nameof(ShortDateExample));
-    }
-  }
+  public IEnumerable<ISettingEditor> SettingEditors => _SettingEditors
+    .GroupBy(e => e.Group)
+    .SelectMany(g => g)
+    .OrderBy(e => e.DisplayName);
 }
