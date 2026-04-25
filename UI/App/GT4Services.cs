e77b8fd53f0e7020dc9db2ee2d9729d50e4a757c
gasizdat <gasizdat@gmail.com>
2026-04-25 21:17:34 +0300

    (*) Add ServiceCollectionExtensions to GT4.UI.Utils. Register all services in this method.

--- UI/App/GT4Services.cs
+++ UI/App/GT4Services.cs
@@ -1,9 +1,6 @@
 ﻿using GT4.Core.Project;
-using GT4.Core.Project.Dto;
 using GT4.Core.Utils;
-using GT4.UI.Utils.Comparers;
-using GT4.UI.Utils.Converters;
-using GT4.UI.Utils.Formatters;
+using GT4.UI.Utils;
 using Microsoft.Extensions.Configuration;
 
 namespace GT4.UI;
@@ -20,18 +17,7 @@ public class GT4Services
     serviceCollection
       .AddSingleton<IConfiguration>(configurationRoot)
       .AddActiveConfigurations(configurationRoot)
-      .AddSingleton<IDateFormatter, DateFormatter>()
-      .AddSingleton<INameFormatter, NameFormatter>()
-      .AddSingleton<IDateSpanFormatter, DateSpanFormatter>()
-      .AddSingleton<IRelationshipTypeFormatter, RelationshipTypeFormatter>()
-      .AddSingleton<INameTypeFormatter, NameTypeFormatter>()
-      .AddSingleton<IBiologicalSexFormatter, BiologicalSexFormatter>()
-      .AddSingleton<IComparer<ProjectInfo>, ProjectInfoComparer>()
-      .AddSingleton<IComparer<PersonInfo>, PersonInfoComparer>()
-      .AddSingleton<IComparer<Name>, NameComparer>()
-      .AddKeyedSingleton<IDataConverter, ImageDataConverter>(DataCategory.PersonPhoto)
-      .AddKeyedSingleton<IDataConverter, ImageDataConverter>(DataCategory.PersonMainPhoto)
-      .AddKeyedSingleton<IDataConverter, TextDataConverter>(DataCategory.PersonBio)
+      .AddUIUtils()
       .AddDefaultUtils()
       .AddDefaultProject();
   }
---
+++ UI/Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using GT4.Core.Project.Dto;
+using GT4.Core.Utils;
+using GT4.UI.Utils.Comparers;
+using GT4.UI.Utils.Converters;
+using GT4.UI.Utils.Formatters;
+using GT4.UI.Utils.Settings;
+
+namespace GT4.UI.Utils;
+
+public static class ServiceCollectionExtensions
+{
+  public static IServiceCollection AddUIUtils(this IServiceCollection services)
+  {
+    return services
+      .AddSingleton<IDateFormatter, DateFormatter>()
+      .AddSingleton<INameFormatter, NameFormatter>()
+      .AddSingleton<IDateSpanFormatter, DateSpanFormatter>()
+      .AddSingleton<IRelationshipTypeFormatter, RelationshipTypeFormatter>()
+      .AddSingleton<INameTypeFormatter, NameTypeFormatter>()
+      .AddSingleton<IBiologicalSexFormatter, BiologicalSexFormatter>()
+      .AddSingleton<IComparer<ProjectInfo>, ProjectInfoComparer>()
+      .AddSingleton<IComparer<PersonInfo>, PersonInfoComparer>()
+      .AddSingleton<IComparer<Name>, NameComparer>()
+      .AddKeyedSingleton<IDataConverter, ImageDataConverter>(DataCategory.PersonPhoto)
+      .AddKeyedSingleton<IDataConverter, ImageDataConverter>(DataCategory.PersonMainPhoto)
+      .AddKeyedSingleton<IDataConverter, TextDataConverter>(DataCategory.PersonBio)
+      .AddKeyedSingleton<ISettingEditor, FullDateFormatSetting>(nameof(FullDateFormatSetting))
+      .AddKeyedSingleton<ISettingEditor, ShortDateFormatSetting>(nameof(ShortDateFormatSetting))
+      ;
+  }
+}
