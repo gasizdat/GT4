3ec95a7ac8afe58dfa433b3944ce27250aae7bc4
gasizdat <gasizdat@gmail.com>
2026-05-03 12:28:25 +0300

    (+) Add MarkdownEditor component.

---
+++ UI/App/Components/MarkdownEditor.xaml.cs
@@ -0,0 +1,86 @@
+using System.Windows.Input;
+
+namespace GT4.UI.Components;
+
+public partial class MarkdownEditor : ContentView
+{
+  private readonly ICommand _Command;
+  private int _TabIndex;
+
+  public MarkdownEditor()
+  {
+    _Command = new SafeCommand(OnCommand);
+    _TabIndex = 0;
+
+    InitializeComponent();
+  }
+
+  public static readonly BindableProperty MarkdownProperty =
+    BindableProperty.Create(nameof(Markdown), typeof(string), typeof(MarkdownView), default, BindingMode.TwoWay, null, OnMarkdownChanged);
+
+
+  public static readonly BindableProperty PlaceholderProperty =
+    BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(MarkdownView), default, BindingMode.OneWay, null, OnPlaceholderChanged);
+
+  public string? Markdown
+  {
+    get => (string?)GetValue(MarkdownProperty);
+    set => SetValue(MarkdownProperty, value);
+  }
+
+  public string? Placeholder
+  {
+    get => (string?)GetValue(PlaceholderProperty);
+    set => SetValue(PlaceholderProperty, value);
+  }
+
+  public bool DisplayEditor => _TabIndex == 0;
+
+  public bool DisplayHtmlView => _TabIndex == 1;
+
+  public int TabIndex
+  {
+    get => _TabIndex;
+    set
+    {
+      if (_TabIndex != value)
+      {
+        _TabIndex = value;
+        OnPropertyChanged(nameof(TabIndex));
+        OnPropertyChanged(nameof(DisplayEditor));
+        OnPropertyChanged(nameof(DisplayHtmlView));
+      }
+    }
+  }
+
+  public ICommand Command => _Command;
+
+  private static void OnMarkdownChanged(BindableObject obj, object oldValue, object newValue)
+  {
+    if (obj is MarkdownEditor markdownEditor && oldValue != newValue)
+    {
+      markdownEditor.OnPropertyChanged(nameof(Markdown));
+    }
+  }
+
+  private static void OnPlaceholderChanged(BindableObject obj, object oldValue, object newValue)
+  {
+    if (obj is MarkdownEditor markdownEditor && oldValue != newValue)
+    {
+      markdownEditor.OnPropertyChanged(nameof(Placeholder));
+    }
+  }
+
+  private void OnCommand(object obj)
+  {
+    switch (obj)
+    {
+      case string commandName when commandName == "Tab0":
+        TabIndex = 0;
+        break;
+      case string commandName when commandName == "Tab1":
+        TabIndex = 1;
+        break;
+    }
+  }
+}
