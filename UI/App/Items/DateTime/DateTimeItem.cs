21ee7f8b079707dd1d0019798e0154bf99c1b710
gasizdat <gasizdat@gmail.com>
2026-05-22 19:17:06 +0300

    (+) Add DateTimeItem

---
+++ UI/App/Items/DateTime/DateTimeItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GT4.UI.Items;
+
+public class DateTimeItem : CollectionItemBase<DateTime>
+{
+  public DateTimeItem(DateTime dateTime)
+    : base(dateTime, string.Empty)
+  {
+  }
+
+  public string DateTimeText => $"{Info.ToLocalTime().ToLongDateString()}, {Info.ToLocalTime().ToLongTimeString()}";
+}
