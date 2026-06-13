e7ed0b01a84d451c088fe6e760856dc729c5a6d4
gasizdat <gasizdat@gmail.com>
2026-06-13 18:59:57 +0300

    (-) Fix null-coalescing precedence in Date.Create
    In Date.Create(year, month, day, status) the expression
    "... + day ?? 0" binds as "(... + day) ?? 0" because + has higher
    precedence than the null-coalescing operator. A null day would
    therefore zero out the whole code (losing year and month) instead of
    only defaulting the day component. Parenthesize as (day ?? 0).
    Currently masked because the only caller passes non-null ints.

--- Core/Utils/Date.cs
+++ Core/Utils/Date.cs
@@ -22,7 +22,7 @@ public struct Date
 
   public static Date Create(int? year, int? month, int? day, DateStatus status)
   {
-    var code = (year ?? 0) * Digit * Digit + (month ?? 0) * Digit + day ?? 0;
+    var code = (year ?? 0) * Digit * Digit + (month ?? 0) * Digit + (day ?? 0);
     return Create(code, status);
   }
 
